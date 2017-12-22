fba905249d49c51390191b37ff10f856e5c88cbc
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-12-22 12:05:18 +0000

    First version

---
+++ Tutorial2_MovingManipulator/UserForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Tutorial2_MovingManipulator
+{
+	partial class UserForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.numericUpDown_RotateAmount = new System.Windows.Forms.NumericUpDown();
+            this.lbl_RotateAmount = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_RotateAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown_RotateAmount
+            // 
+            this.numericUpDown_RotateAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown_RotateAmount.Location = new System.Drawing.Point(160, 36);
+            this.numericUpDown_RotateAmount.Name = "numericUpDown_RotateAmount";
+            this.numericUpDown_RotateAmount.Size = new System.Drawing.Size(92, 22);
+            this.numericUpDown_RotateAmount.TabIndex = 0;
+            this.numericUpDown_RotateAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown_RotateAmount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_RotateAmount.ValueChanged += new System.EventHandler(this.numericUpDown_RotateAmount_ValueChanged);
+            // 
+            // lbl_RotateAmount
+            // 
+            this.lbl_RotateAmount.AutoSize = true;
+            this.lbl_RotateAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_RotateAmount.Location = new System.Drawing.Point(31, 38);
+            this.lbl_RotateAmount.Name = "lbl_RotateAmount";
+            this.lbl_RotateAmount.Size = new System.Drawing.Size(99, 16);
+            this.lbl_RotateAmount.TabIndex = 1;
+            this.lbl_RotateAmount.Text = "Rotate Amount:";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.Location = new System.Drawing.Point(13, 110);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(259, 61);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lbl_RotateAmount);
+            this.Controls.Add(this.numericUpDown_RotateAmount);
+            this.Name = "UserForm";
+            this.Text = "IPC Tutorial 2: Moving the Manipulator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserForm_FormClosing);
+            this.Load += new System.EventHandler(this.UserForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_RotateAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown_RotateAmount;
+        private System.Windows.Forms.Label lbl_RotateAmount;
+        private System.Windows.Forms.Button btn_Start;
+	}
+}
+
---
+++ Tutorial2_MovingManipulator/UserForm.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using IpcContractClientInterface;
+using AppLog = IpcUtil.Logging;
+using System.Globalization;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace Tutorial2_MovingManipulator
+{
+	public partial class UserForm : Form
+	{
+		/// <summary>Are we in design mode</summary>
+		protected bool mDesignMode { get; private set; }
+
+		#region Standard IPC Variables
+
+		/// <summary>This ensures consistent read and write culture</summary>
+		private NumberFormatInfo mNFI = new CultureInfo("en-GB", true).NumberFormat; // Force UN English culture
+
+		/// <summary>Collection of all IPC channels, this object always exists.</summary>
+		private Channels mChannels = new Channels();
+
+		#endregion Standard IPC Variables
+
+        #region Application Variables
+
+        /// <summary> Status of the application </summary>
+        private Channels.EConnectionState mApplicationState;
+
+        /// <summary> Define the rotate axis (constant) </summary>
+        const IpcContract.Manipulator.EAxisName mRotateAxis = IpcContract.Manipulator.EAxisName.Rotate;
+
+        /// <summary> Go signal sent flag </summary>
+        private bool mManipulatorGoSignalSent = false;
+        /// <summary> Go complete flag </summary>
+        private bool mManipulatorGoComplete = false;
+
+        /// <summary> Rotate amount </summary>
+        private decimal mRotateAmount = 10;
+
+        /// <summary> Manipulator thread variable </summary>
+        private Thread mManipulatorThread = null;
+
+        #endregion Application Variables
+
+        public UserForm()
+		{
+			try
+			{
+				mDesignMode = (LicenseManager.CurrentContext.UsageMode == LicenseUsageMode.Designtime);
+				InitializeComponent();
+				if (!mDesignMode)
+				{
+					// Tell normal logging who the parent window is.
+					AppLog.SetParentWindow = this;
+					AppLog.TraceInfo = true;
+					AppLog.TraceDebug = true;
+
+					mChannels = new Channels();
+					// Enable the channels that will be controlled by this application.
+					// For the generic IPC client this is all of them!
+					// This just sets flags, it does not actually open the channels.
+					mChannels.AccessApplication = true;
+					mChannels.AccessXray = false;
+					mChannels.AccessManipulator = true;
+					mChannels.AccessImageProcessing = false;
+                    mChannels.AccessInspection = false;
+					mChannels.AccessInspection2D = false;
+					mChannels.AccessCT3DScan = false;
+					mChannels.AccessCT2DScan = false;
+				}
+			}
+			catch (Exception ex) { AppLog.LogException(ex); }
+		}
+
+		#region Channel connections
+
+		/// <summary>Attach to channel and connect any event handlers</summary>
+		/// <returns>Connection status</returns>
+		private Channels.EConnectionState ChannelsAttach()
+		{
+			try
+			{
+				if (mChannels != null)
+				{
+					Channels.EConnectionState State = mChannels.Connect();
+					if (State == Channels.EConnectionState.Connected)  // Open channels
+					{
+						// Attach event handlers (as required)
+
+						if (mChannels.Application != null)
+						{
+							mChannels.Application.mEventSubscriptionHeartbeat.Event +=
+								new EventHandler<CommunicationsChannel_Application.EventArgsHeartbeat>(EventHandlerHeartbeatApp);
+						}
+
+
+						if (mChannels.Manipulator != null)
+						{
+							mChannels.Manipulator.mEventSubscriptionHeartbeat.Event +=
+								new EventHandler<CommunicationsChannel_Manipulator.EventArgsHeartbeat>(EventHandlerHeartbeatMan);
+							mChannels.Manipulator.mEventSubscriptionManipulatorMove.Event +=
+								new EventHandler<CommunicationsChannel_Manipulator.EventArgsManipulatorMoveEvent>(EventHandlerManipulatorMoveEvent);
+						}
+
+					}
+					return State;
+				}
+			}
+			catch (Exception ex) { AppLog.LogException(ex); }
+			return Channels.EConnectionState.Error;
+		}
+
+		/// <summary>Detach channel and disconnect any event handlers</summary>
+		/// <returns>true if OK</returns>
+		private bool ChannelsDetach()
+		{
+			try
+			{
+				if (mChannels != null)
+				{
+					// Detach event handlers
+
+					if (mChannels.Application != null)
+					{
+						mChannels.Application.mEventSubscriptionHeartbeat.Event -=
+							new EventHandler<CommunicationsChannel_Application.EventArgsHeartbeat>(EventHandlerHeartbeatApp);
+					}
+
+
+					if (mChannels.Manipulator != null)
+					{
+						mChannels.Manipulator.mEventSubscriptionHeartbeat.Event -=
+							new EventHandler<CommunicationsChannel_Manipulator.EventArgsHeartbeat>(EventHandlerHeartbeatMan);
+						mChannels.Manipulator.mEventSubscriptionManipulatorMove.Event -=
+							new EventHandler<CommunicationsChannel_Manipulator.EventArgsManipulatorMoveEvent>(EventHandlerManipulatorMoveEvent);
+					}
+
+					Thread.Sleep(100); // A breather for events to finish!
+					return mChannels.Disconnect(); // Close channels
+				}
+			}
+			catch (Exception ex) { AppLog.LogException(ex); }
+			return false;
+		}
+
+		#endregion Channel connections
+
+		#region Heartbeat from host
+
+		void EventHandlerHeartbeatApp(object aSender, CommunicationsChannel_Application.EventArgsHeartbeat e)
+		{
+			try
+			{
+				if (mChannels == null || mChannels.Application == null)
+					return;
+				if (this.InvokeRequired)
+					this.BeginInvoke((MethodInvoker)delegate { EventHandlerHeartbeatApp(aSender, e); });
+				else
+				{
+					//your code goes here....
+				}
+			}
+			catch (ObjectDisposedException) { } // ignore
+			catch (Exception ex) { AppLog.LogException(ex); }
+		}
+
+		void EventHandlerHeartbeatMan(object aSender, CommunicationsChannel_Manipulator.EventArgsHeartbeat e)
+		{
+			try
+			{
+				if (mChannels == null || mChannels.Manipulator == null)
+					return;
+				if (this.InvokeRequired)
+					this.BeginInvoke((MethodInvoker)delegate { EventHandlerHeartbeatMan(aSender, e); });
+				else
+				{
+					//your code goes here....
+				}
+			}
+			catch (ObjectDisposedException) { } // ignore
+			catch (Exception ex) { AppLog.LogException(ex); }
+		}
+
+		#endregion Heartbeat from host
+
+		#region STATUS FROM HOST
+
+		#region Manipulator
+
+		void EventHandlerManipulatorMoveEvent(object aSender, CommunicationsChannel_Manipulator.EventArgsManipulatorMoveEvent e)
+		{
+			try
+			{
+				if (mChannels == null || mChannels.Manipulator == null)
+					return;
+				if (this.InvokeRequired)
+					this.BeginInvoke((MethodInvoker)delegate { EventHandlerManipulatorMoveEvent(aSender, e); }); // Make it non blocking if called form this UI thread
+				else
+				{
+                    Debug.Print(DateTime.Now.ToString("dd/MM/yyyy H:mm:ss.fff") + " : e.MoveEvent=" + e.MoveEvent.ToString());
+					
+                    switch (e.MoveEvent)
+					{
+					case IpcContract.Manipulator.EMoveEvent.HomingStarted:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.HomingCompleted:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.ManipulatorStartedMoving:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.ManipulatorStoppedMoving:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.FilamentChangePositionGoStarted:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.GoCompleted:
+                        // Set mManipulatorGoComplete flag to be true
+                        mManipulatorGoComplete = true;
+						break;
+					case IpcContract.Manipulator.EMoveEvent.GoStarted:
+                        // Set mManipulatorGoSignalSent flag to be true 
+                        mManipulatorGoSignalSent = true;
+						break;
+					case IpcContract.Manipulator.EMoveEvent.LoadPositionGoCompleted:
+                        // Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.LoadPositionGoStarted:
+						// Your code goes here...
+						break;
+					case IpcContract.Manipulator.EMoveEvent.Error:
+						// Your code goes here...
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			catch (Exception ex) { AppLog.LogException(ex); }
+		}
+
+
+		#endregion
+
+
+        
+        #endregion Status from host
+
+        #region User functions
+
+        private void UserForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Attach channels
+                mApplicationState = ChannelsAttach();
+
+                if (mApplicationState == Channels.EConnectionState.Connected)
+                    Debug.Print(DateTime.Now.ToString("dd/MM/yyyy H:mm:ss.fff") + " : Connected to Inspect-X");
+                else
+                    Debug.Print(DateTime.Now.ToString("dd/MM/yyyy H:mm:ss.fff") + " : Problem in connecting to Inspect-X");
+            }
+            catch (Exception ex) { AppLog.LogException(ex); }
+        }
+
+        
+
+        private void UserForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                // Detach channels
+                ChannelsDetach();
+
+                Debug.Print(DateTime.Now.ToString("dd/MM/yyyy H:mm:ss.fff") + " : Disconnected from Inspect-X");
+            }
+            catch (Exception ex) { AppLog.LogException(ex); }
+        }
+        
+
+        private void numericUpDown_RotateAmount_ValueChanged(object sender, EventArgs e)
+        {
+            mRotateAmount = numericUpDown_RotateAmount.Value;
+        }
+        
+        
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            // Initialise a new thread for Manipulator move to run on
+            mManipulatorThread = new Thread(ManipulatorMove);
+            // Start the thread
+            mManipulatorThread.Start();
+        }
+
+
+        #endregion User functions
+
+
+        #region Manipulator routines
+
+        private void ManipulatorMove()
+        {
+            // If ApplicationState is not connected then immediately exit the routine
+            if (mApplicationState != Channels.EConnectionState.Connected)
+                return;
+
+            // For safety, disable the Start button
+            this.Invoke((MethodInvoker)delegate 
+            { 
+                btn_Start.Enabled = false;
+                lbl_RotateAmount.Enabled = false;
+                numericUpDown_RotateAmount.Enabled = false;
+            });
+
+            // Variable for current manipulator position
+            float aManipulatorCurrentPosition;
+
+            // Variable for demanded manipulator position
+            float aManipulatorDemandedPosition;
+
+            // Set Movement flags to be false
+            mManipulatorGoSignalSent = false;
+            mManipulatorGoComplete = false;
+
+            // Look up current position
+            aManipulatorCurrentPosition = mChannels.Manipulator.Axis.Position(mRotateAxis);
+
+            // Calculate new demanded position
+            aManipulatorDemandedPosition = aManipulatorCurrentPosition + (float)mRotateAmount;
+
+            // Set target position of rotate axis to be demanded position
+            mChannels.Manipulator.Axis.Target(mRotateAxis, aManipulatorDemandedPosition);
+
+            // Tell manipulator to move
+            mChannels.Manipulator.Axis.Go(mRotateAxis);
+
+            // while both movement flags are still not positive, then wait
+            while (!mManipulatorGoSignalSent || !mManipulatorGoComplete)
+                Thread.Sleep(10);
+
+            // Re-enable the Start button
+            this.Invoke((MethodInvoker)delegate
+            {
+                btn_Start.Enabled = true;
+                lbl_RotateAmount.Enabled = true;
+                numericUpDown_RotateAmount.Enabled = true;
+            });
+        }
+
+        #endregion Manipulator routines
+
+
+
+
+    }
+}
